b724ed276408f4524199172f93834a5950bfb761
Yoni Nazarathy <yoninazarathy@gmail.com>
2021-08-06 11:41:37 +1000

    mwe

--- multi_devel/play_test.jl
+++ multi_devel/play_test.jl
@@ -1,5 +1,9 @@
-using Pkg
+using Pkg, Random, Distributions
 cd(@__DIR__)
 Pkg.activate(".")
 @show pwd()
 include("../src/EMpht_multi.jl")
+
+Random.seed!(1)
+sObs = Sample(obs=rand(Exponential(), 1_000))
+ph1unif = empht(sObs, p=1, method=:unif)
