1d14c77f4fcecb0de50f34b9ee93f609824252fd
yoninazarathy <y.nazarathy@uq.edu.au>
2021-08-16 13:16:01 +1000

    probability matrix

--- multi_devel/em_fit_raw.jl
+++ multi_devel/em_fit_raw.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 mutable struct MAPHParameters
     T::Matrix{Float64}
     T0::Matrix{Float64}
@@ -26,6 +28,20 @@ function update_sufficent_stats(maph::MAPHParameters, data::MAPHObsData)
     #QQQQ - Here start to add code to update the sufficents stats
 end
 
+mutable struct ProbabilityMatrix
+    P::Matrix{Float64} # transient to transient
+    P0::Matrix{Float64}#transient to absorbing
+    function ProbabilityMatrix(maph::MAPHParameters)
+        p, q = model_size(maph)
+        new(zeros(p,p), zeros(p,q))
+    end
+end
+
+function update_probability_matrix(maph::MAPHParameters, probmatrix::ProbabilityMatrix)
+    probmatrix.P = I-inv(Diagonal(maph.T))*maph.T
+    probmatrix.P0 = -inv(Diagonal(maph.T))*maph.T0
+end
+
 
 function test_example()
     Λ₄, λ45, λ54, Λ₅ = 3, 2, 7, 10
@@ -34,11 +50,13 @@ function test_example()
     T0_example = [μ41 μ42 μ43; μ51 μ52 μ53]
 
     maph = MAPHParameters(T_example, T0_example)
+    probmatrix = ProbabilityMatrix(maph)
     @show model_size(maph)
     @show SufficientStats(maph)
 
     data = [(y=2.3,a=2),(y=5.32,a=1),(y=15.32,a=2)]
-    update_sufficent_stats(maph,data)
+    update_sufficent_stats(maph, data)
+    update_probability_matrix(maph,probmatrix)
 end
 
 test_example()
